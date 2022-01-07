7f783a6fc71744b13a09ee6af78a0a5f6e6cb56d
Gunasethu <gunasethu197@gmail.com>
2022-01-07 11:47:08 -0600

    updated

--- app.py
+++ app.py
@@ -8,25 +8,23 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def home():
+def temp():
     return render_template('home.html')
 
-@app.route('/prediction', methods=['POST'])
-def prediction():
-    if request.method == 'POST':
-        TSH =(request.form["TSH"])
-        FTI =(request.form["FTI"])
-        TT4 =(request.form["TT4"])
-        T3 =(request.form["T3"])
-        query_hypothyroid =(request.form["query_hypothyroid"])
-        on_thyroxine =(request.form["on_thyroxine"])
-        sex =(request.form["sex"])
-        pregnant =(request.form["pregnant"])
-        psych =(request.form["psych"])
-        arr=np.array([[TSH, FTI ,TT4, T3,0,0,0,0,0]])
+@app.route('/predict', methods=['POST'])
+def home():
+        TSH = request.form["TSH"]
+        FTI = request.form["FTI"]
+        TT4 = request.form["TT4"]
+        T3 = request.form["T3"]
+        query_hypothyroid = request.form["query_hypothyroid"]
+        on_thyroxine = request.form["on_thyroxine"]
+        sex = request.form["sex"]
+        pregnant = request.form["pregnant"]
+        psych = request.form["psych"]
+        arr=np.array([[TSH, FTI ,TT4, T3,query_hypothyroid, on_thyroxine, sex, pregnant, psych]])
         prediction=model.predict(arr)
-
-    return render_template('after.html',data=prediction)
+        return render_template('after.html',data=prediction)
 
 if __name__ == '__main__':
     app.run(debug=True)
