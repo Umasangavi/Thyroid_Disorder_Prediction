953ac5c7513f98419b632506f88973a350b27faf
Gunasethu <gunasethu197@gmail.com>
2022-01-06 22:43:31 -0600

    deployed

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+import joblib
+import numpy as np
+
+model = joblib.load("models/model.pkl")
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/prediction', methods=['POST'])
+def prediction():
+    if request.method == 'POST':
+        TSH =(request.form["TSH"])
+        FTI =(request.form["FTI"])
+        TT4 =(request.form["TT4"])
+        T3 =(request.form["T3"])
+        query_hypothyroid =(request.form["query_hypothyroid"])
+        on_thyroxine =(request.form["on_thyroxine"])
+        sex =(request.form["sex"])
+        pregnant =(request.form["pregnant"])
+        psych =(request.form["psych"])
+        arr=np.array([[TSH, FTI ,TT4, T3,0,0,0,0,0]])
+        prediction=model.predict(arr)
+
+    return render_template('after.html',data=prediction)
+
+if __name__ == '__main__':
+    app.run(debug=True)
